b9016a72fdc63cb928903e3031d8fe15a9696771
Dr. Juan Gabriel Colonna <juancolonna@gmail.com>
2023-09-05 20:01:03 -0400

    Add files via upload

--- R/SigmaAR.R
+++ R/SigmaAR.R
@@ -1,12 +1,25 @@
 #' @title Simulate Covariance Matrix with an Auto-regressive (AR) Model
 #'
-#' @description Helper function to simulate synthetic data
+#' @description Helper function to simulate Simulate Gaussian Data with an Autoregressive (AR) Model
 #'
 #' @param p Number of variables (rows x columns).
 #' @param phi Auto-regression coefficient.
 #'
 #' @return A list with the values:
 #' \item{\code{Sigma}}{A covariance matrix.}
+#'
+#' @examples
+#' library(FastStepGraph)
+#' library(MASS) # mvrnorm
+#' phi = 0.4
+#' p = 100  # Dimension
+#' n = 100 # Sample size
+#' Sigma = SigmaAR(p, phi) # Simulate Gaussian Data with Autoregressive (AR) Model
+#' Omega = solve(Sigma)
+#' Omega[abs(Omega) < 1e-5] = 0
+#' X = list() # Generate Data from a Gaussian distribution
+#' X = mvrnorm(n, mu=rep(0,p), Sigma)
+#' X = scale(X)
 #'
 #' @author Prof. Juan G. Colonna, PhD. \email{juancolonna@icomp.ufam.edu.br}
 #' @author Prof. Marcelo Ruiz, PhD. \email{mruiz@exa.unrc.edu.ar}
